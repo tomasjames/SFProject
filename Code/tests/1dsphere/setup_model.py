3fe8c5c7d2a34123365e2fc261abdcd4d0be02a5
Tomas James <tomas.james@me.com>
2015-10-10 11:36:26 +0100

    WORKING SPHERE IN 1D :

--- Code/tests/1dsphere/setup_model.py
+++ Code/tests/1dsphere/setup_model.py
@@ -11,6 +11,9 @@ import radmc3dPy
 # Import OS manipulation tools
 import os
 
+# Import matplotlib
+import matplotlib.pyplot as plt
+
 # Write a parameter file containing all of the default parameters
 radmc3dPy.analyze.writeDefaultParfile('spher1d_1')
 
@@ -20,3 +23,12 @@ radmc3dPy.setup.problemSetupDust('spher1d_1', binary=False)
 # Copy the dust opacity and data files from the datafiles directory
 os.system('cp -v ../datafiles/dustkappa_silicate.inp .')
 os.system('cp -v ../datafiles/molecule_co.inp .')
+
+# Run thermal Monte-Carlo simulation
+os.system('radmc3d mctherm')
+
+# Plot image
+radmc3dPy.image.makeImage(npix=400, sizeau=400, wav=800., incl=90., posang=43.)
+
+imag = radmc3dPy.image.readImage()
+radmc3dPy.image.plotImage(imag, arcsec=True, dpc=140., log=True, maxlog=5)
