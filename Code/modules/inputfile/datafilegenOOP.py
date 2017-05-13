01acc40b1a17ce0b6850220b345115585f7b9574
Tomas James <tomas.james@me.com>
2017-05-13 19:05:01 +0100

    amr_grid.inp is now written by OOP code successfully

--- Code/modules/inputfile/datafilegenOOP.py
+++ Code/modules/inputfile/datafilegenOOP.py
@@ -4,22 +4,43 @@ import numpy as np
 # Import constants from radmc3dPy
 from radmc3dPy.natconst import *
 
-class DataFileGen(width, ncell):
 
-	def __init__(self, width, ncell):
-		self.width = width
-		self.ncell = ncell
+class DataFileGen:
 
-	def amr_grid(self):
-		############################ Define axes to work with ##########################
-        # Define the image width in cm and au
-        dpix_cm = width*au
-        dpix_au = (dpix_au)/ncell
+    def __init__(self, width, ncell):
+        self.width = width
+        self.ncell = ncell
 
-        # Create a dictionary of pixels in each dimension
-        grid = {'x': np.linspace(-(dpix_cm)/2,(dpix_cm)/2,ncell+1), 'y': np.linspace(-(dpix_cm)/2,(dpix_cm)/2,ncell+1), 'z': np.linspace(-(dpix_cm)/2,(dpix_cm)/2,ncell+1)}
-        
-        ######################## Write file to working directory #######################
-        # Writes a file called amr_grid.inp to the working directory and allows it to be
-        # written to
-        amr = open('amr_grid.inp', 'w')
+        self.image_width_cm = self.width*au # Converts width in au to width in cm
+        self.cell_width_cm = self.image_width_cm/self.ncell # Determines the width of a cell in cm
+
+    def dimensions(self): 
+        '''
+        Returns a dictionary of pixels in x, y and z dimensions
+        '''
+        return {
+            'x': np.linspace(-(self.image_width_cm)/2,(self.image_width_cm)/2,self.ncell+1), 
+            'y': np.linspace(-(self.image_width_cm)/2,(self.image_width_cm)/2,self.ncell+1), 
+            'z': np.linspace(-(self.image_width_cm)/2,(self.image_width_cm)/2,self.ncell+1)
+        }
+
+    def write_amr_grid(self):
+
+        amr = open('amr_grid.inp', 'w') # Writes file to the current directory
+        amr.write('1 # The same as most other files\n') # Write the iformat number
+        amr.write('0 # 0 = regular\n') # Write the grid style
+        amr.write('1 # < 100 = Cartesian\n') # Write coordinate system
+        amr.write('0 # If 1, abundant information is written regarding the grid\n') # Write gridinfo integer
+        amr.write('1 1 1 \n') # Write integers to activate dimensions
+        amr.write(str(self.ncell)+ ' ' + str(self.ncell) + ' ' +str(self.ncell) + '\n') # Write the number of base cells in each dimension
+
+        for dimension in self.dimensions():
+            print dimension
+            for i in self.dimensions()[dimension]:
+                if i == self.dimensions()[dimension][-1]:
+                    print 'In the last pixel'
+                    amr.write(str(np.float64(i)) + str('\n'))
+                else:
+                    amr.write(str(np.float64(i)) + str(' '))
+
+        amr.close()
