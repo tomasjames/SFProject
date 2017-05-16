5745b19e8d118a1d2e2efdd134dc12f8fb4618a7
Tomas James <tomas.james@me.com>
2017-05-16 22:14:51 +0100

    OOP code now accounts for transmission. Next step is to build up saving this to FITS and convolution routine

--- Code/modules/inputfile/datafilegenOOP.py
+++ Code/modules/inputfile/datafilegenOOP.py
@@ -14,6 +14,8 @@ class DataFileGen:
         self.image_width_cm = self.width*au # Converts width in au to width in cm
         self.cell_width_cm = self.image_width_cm/self.ncell # Determines the width of a cell in cm
 
+        self.grid_center = [0,0,0]
+
     def dimensions(self): 
         '''
         Returns a dictionary of pixels in x, y and z dimensions
@@ -35,12 +37,7 @@ class DataFileGen:
         amr.write(str(self.ncell)+ ' ' + str(self.ncell) + ' ' +str(self.ncell) + '\n') # Write the number of base cells in each dimension
 
         for dimension in self.dimensions():
-            print dimension
             for i in self.dimensions()[dimension]:
-                if i == self.dimensions()[dimension][-1]:
-                    print 'In the last pixel'
-                    amr.write(str(np.float64(i)) + str('\n'))
-                else:
-                    amr.write(str(np.float64(i)) + str(' '))
-
+                amr.write(str(np.float64(i)) + str('\n')) if i == self.dimensions()[dimension][-1] else amr.write(str(np.float64(i)) + str(' ')) # Ternary operator to write new line when code reaches end of x plane, for example
         amr.close()
+
